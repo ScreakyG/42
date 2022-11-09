aefe171f86e24c36f51583007ba6795b06b260d8
fgonzale <francois.gnz@gmail.com>
2022-11-09 13:05:22 +0100

    dossier

---
+++ libft/ft_atoi.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: francois <francois@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 12:37:49 by francois          #+#    #+#             */
+/*   Updated: 2022/11/09 13:02:27 by francois         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	signe;
+	int	resultat;
+
+	i = 0;
+	signe = 1;
+	resultat = 0;
+
+	while (str[i] == 32 || str[i] == 13 || str[i] == 9)
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			signe *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		resultat *= 10;
+		resultat += str[i] - '0';
+		i++;
+	}
+	return (resultat * signe);	
+}
